8722b34b5e98a673eb7b274045403269a76d92c7
Venali Soni <sonivenali.vs@gmail.com>
2021-03-06 17:55:58 +0530

    added Other food item

--- lib/Screens/home_screen_body.dart
+++ lib/Screens/home_screen_body.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:foodapp/models/home_data.dart';
 import 'package:foodapp/services/services.dart';
 
-
 class HomeBody extends StatefulWidget {
   @override
   _HomeBodyState createState() => _HomeBodyState();
@@ -32,82 +31,144 @@ class _HomeBodyState extends State<HomeBody> {
   }
 
   Widget buildBody(HomeData data) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 24, right: 24, top: 30),
-          child: TextField(
-            cursorColor: Colors.grey,
-            decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.black,
-              ),
-              hintText: "Search food",
-              contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 8),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(60),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24, top: 30),
+            child: TextField(
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                hintText: "Search food",
+                contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(60),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(26),
-          child: Text(
-            "Recommended",
-            style: TextStyle(
-                color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600),
+          Padding(
+            padding: const EdgeInsets.all(26),
+            child: Text(
+              "Recommended",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600),
+            ),
           ),
-        ),
-        Container(
-          height: 250,
-          padding: EdgeInsets.only(left: 16),
-          child: ListView.builder(
-            itemCount: data.recommended.length,
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 160,
-                padding: const EdgeInsets.all(8.0),
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 250,
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(16),
-                          child: Image.network(
-                            data.recommended[index].image,
-                            fit: BoxFit.cover,
-                          )),
-                    ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(16),
-                      child: Container(
-                        color: Color.fromRGBO(0, 0, 0, 0.4),
+          Container(
+            height: 250,
+            padding: EdgeInsets.only(left: 16),
+            child: ListView.builder(
+              itemCount: data.recommended.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 160,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 250,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.network(
+                              data.recommended[index].image,
+                              fit: BoxFit.cover,
+                            )),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8, left: 8),
-                      child: Text(
-                        data.recommended[index].name,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Container(
+                          color: Color.fromRGBO(0, 0, 0, 0.4),
                         ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8, left: 8),
+                        child: Text(
+                          data.recommended[index].name,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 26,
+              left: 26,
+              bottom: 8,
+            ),
+            child: Text(
+              "Other Food",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          ListView.builder(
+            itemCount: data.otherFood.length,
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return Container(
+                height: 150,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8, left: 8),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(24),
+                          child: Image.network(
+                            data.otherFood[index].image,
+                            width: 100,
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "xl",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          Text(
+                            data.otherFood[index].name,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             },
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
